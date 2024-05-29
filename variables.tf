26e957ad403b7100231a8ba8d4136bc0587da043
Bcloud08 <168526354+Bcloud08@users.noreply.github.com>
2024-05-29 20:15:51 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "filename" {
+    default = "pets.txt"
+    type = string
+}
+variable "content" {
+  default = "I love Banana"
+}
+variable "prefix" {
+    default = "MR"
+    type = string
+  
+}
+variable "length" {
+    default = "2"
+    type = number
+  
+}
